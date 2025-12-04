1cab344be2acb2e1898c0d66651d4ce5c3c07df1
U1927 <64768873+U1927@users.noreply.github.com>
2025-12-04 23:06:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 // Supabase setup
 const supabaseUrl = 'https://xyzcompany.supabase.co';
